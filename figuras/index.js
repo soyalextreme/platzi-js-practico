8b0b935a3c6561b266c638472b2dac2a4f9d438f
soyalextreme <soyalextreme@gmail.com>
2021-07-18 21:14:22 -0500

    [add] Figuras funciones e interaccion con el dom

--- figuras/index.js
+++ figuras/index.js
@@ -1,47 +1,81 @@
-// codigo cuadrado
-console.group("Cuadrado");
-const sideSquare = 5;
-console.log("Los lados del cuadrado mide: ", sideSquare + "cm");
+function perimeterSquare(side) {
+  return side * 4;
+}
 
-const perimeterSquare = sideSquare * 4;
-console.log("Perimeter: ", perimeterSquare, "cm");
+function areaSquare(side) {
+  return side * side;
+}
 
-const areaSquare = sideSquare * sideSquare;
-console.log("Area: ", areaSquare, "cm2");
-console.groupEnd();
+const getHeight = (side, base) =>
+  Math.sqrt(Math.pow(side, 2) - Math.pow(base / 2, 2));
 
-// codigo cuadrado
-const ladoTrianguloA = 6;
-const ladoTriaguloB = 6;
-const baseTriangulo = 4;
+const areaTriangle = (side, base) => (base * getHeight(side, base)) / 2;
+const perimeterTriangle = (side1, side2, base) => side1 + side2 + base;
 
-console.group("Triangulo");
-console.log(
-  `Lado A: ${ladoTrianguloA}cm\nLado B: ${ladoTriaguloB}cm\nBase: ${baseTriangulo}cm`
-);
+const perimeterCircle = (radius) => {
+  const diameter = radius * 2;
+  return diameter * 3.1416;
+};
+const areaCircle = (radius) => 3.1416 * Math.pow(radius, 2);
 
-const alturaTriangulo = 5.5;
-console.log("Altura: ", alturaTriangulo, "cm");
-const perimetroTriangulo = ladoTrianguloA + ladoTriaguloB + baseTriangulo;
-console.log(`Perimetro: ${perimeterSquare} cm`);
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log(`Area: ${areaTriangulo} cm`);
+const calculatePerimeterSquare = () => {
+  const side = document.querySelector("#square-side-input").value;
+  const perimeterPrinter = document.querySelector(
+    "#result-text-square-perimeter"
+  );
+  perimeterPrinter.innerHTML = `Result Perimeter: ${perimeterSquare(side)}`;
+};
 
-console.groupEnd();
+const calculateAreaSquare = () => {
+  const side = document.querySelector("#square-side-input").value;
+  const areaPrinter = document.querySelector("#result-text-square-area");
+  areaPrinter.innerHTML = `Result Perimeter: ${areaSquare(side)}`;
+};
 
-// codigo circulo
-console.group("Cicle");
-const radio = 4;
-const diametro = 4 * 2;
-const PI = Math.PI;
-const perimeterCircle = diametro * PI;
-const areaCircle = PI * (radio * radio);
-console.log(`
-    Radio: ${radio} cm
-    Diameter: ${diametro}cm
-    PI: ${PI}
-    Perimetro: ${perimeterCircle} cm
-    Area: ${areaCircle} cm2
-`);
+const getSelectorsTriangle = () => {
+  const sideA = document.querySelector("#triangle-sideA-input").value;
+  const sideB = document.querySelector("#triangle-sideB-input").value;
+  const base = document.querySelector("#triangle-base-input").value;
+  console.log(sideA, sideB, base);
+  const printerArea = document.querySelector("#result-text-triangle-area");
+  const printerPerimeter = document.querySelector(
+    "#result-text-triangle-perimeter"
+  );
+  return {
+    sideA,
+    sideB,
+    base,
+    printerArea,
+    printerPerimeter,
+  };
+};
 
-console.groupEnd();
+const calculatePerimeterTriangle = () => {
+  const { sideA, sideB, base, printerPerimeter } = getSelectorsTriangle();
+  const hipo = Math.sqrt(Math.pow(sideB, 2) + Math.pow(base, 2));
+  document.querySelector("#triangle-sideA-input").setAttribute("value", hipo);
+  printerPerimeter.innerHTML = `Result perimeter: ${perimeterTriangle(
+    sideA,
+    sideB,
+    base
+  )}`;
+};
+
+const calculateAreaTriangle = () => {
+  const { sideB, base, printerArea } = getSelectorsTriangle();
+  const hipo = Math.sqrt(Math.pow(sideB, 2) + Math.pow(base, 2));
+  document.querySelector("#triangle-sideA-input").setAttribute("value", hipo);
+  printerArea.innerHTML = `Result area: ${areaTriangle(sideB, base)}`;
+};
+
+const calculateCircle = () => {
+  const radius = document.querySelector("#radius-input").value;
+  const area = areaCircle(radius);
+  const perimeter = perimeterCircle(radius);
+  document.querySelector(
+    "#result-text-circle-perimeter"
+  ).innerHTML = `Perimeter Result: ${perimeter}`;
+  document.querySelector(
+    "#result-text-circle-area"
+  ).innerHTML = `Area result ${area}`;
+};
